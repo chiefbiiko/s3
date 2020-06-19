93cb32cbf22883118541329765433e3ff4f5b453
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-06-19 08:10:58 +0200

    deps: update dependencies to run with deno@v1

--- deps.ts
+++ deps.ts
@@ -3,18 +3,18 @@ export {
   fromUint8Array as base64FromUint8Array
 } from "https://deno.land/x/base64@v0.2.0/mod.ts";
 
-export { contentType } from "https://deno.land/std@v0.35.0/media_types/mod.ts";
+export { contentType } from "https://deno.land/x/media_types@v2.3.6/mod.ts";
 
 export {
   encode,
   decode
 } from "https://denopkg.com/chiefbiiko/std-encoding@v1.0.0/mod.ts";
 
-export { extname } from "https://deno.land/std@v0.35.0/path/mod.ts";
+export { extname } from "https://deno.land/std/path/mod.ts";
 
 export {
   get
-} from "https://denopkg.com/chiefbiiko/get-aws-config@v0.3.5/mod.ts";
+} from "https://denopkg.com/chiefbiiko/get-aws-config@v1.0.1/mod.ts";
 
 export { hmac } from "https://denopkg.com/chiefbiiko/hmac@v1.0.2/mod.ts";
 
--- test/deps.ts
+++ test/deps.ts
@@ -1,1 +1,1 @@
-export { assertEquals } from "https://deno.land/std@v0.35.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std/testing/asserts.ts";
