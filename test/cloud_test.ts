816913be98781c656ede23b7bc003aa6f9818662
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2020-07-02 11:02:02 +0200

    test: batch delete object test case

--- test/cloud_test.ts
+++ test/cloud_test.ts
@@ -2,26 +2,28 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 
 import { S3Client, createClient } from "../mod.ts";
 
-import { Doc} from "../util.ts"
+import { Doc } from "../util.ts";
 
-const BUCKET_NAME: string = Deno.env.get("BUCKET_NAME") || "s3testingstack-bucket-419";
+const BUCKET_NAME: string = Deno.env.get("BUCKET_NAME") ||
+  "s3testingstack-bucket-419";
 
 const s3: S3Client = createClient({ bucket: BUCKET_NAME });
 
 Deno.test({
   name: "creating a bucket",
-async fn(): Promise<void> {
-  await s3.createBucket({ Bucket: BUCKET_NAME })
-}})
+  async fn(): Promise<void> {
+    await s3.createBucket({ Bucket: BUCKET_NAME });
+  },
+});
 
 Deno.test({
   name: "PutObject into a bucket defined when constructing the S3 client",
   async fn(): Promise<void> {
-    const result: undefined |Doc = await s3.putObject({
+    const result: undefined | Doc = await s3.putObject({
       Key: ".gitignore",
-      Body: '.env',
+      Body: ".env",
     });
-    
+
     assertEquals(result, undefined);
   },
 });
@@ -29,12 +31,12 @@ Deno.test({
 Deno.test({
   name: "PutObject into a bucket specified at calltime",
   async fn(): Promise<void> {
-    const result: undefined |Doc = await createClient().putObject({
+    const result: undefined | Doc = await createClient().putObject({
       Bucket: BUCKET_NAME,
       Key: "callback.js",
-      Body: 'function cb() {}',
+      Body: "function cb() {}",
     });
-    
+
     assertEquals(result, undefined);
   },
 });
@@ -51,10 +53,20 @@ Deno.test({
 //   }
 // });
 
-// TODO: test case:  empty bucket in order 2 delete it
+Deno.test({
+  name: "batch deleting objects",
+  async fn(): Promise<void> {
+    const Objects = (await s3.listObjectsV2().promise())
+      .Contents
+      .map(({ Key }) => ({ Key }));
+
+    await s3.deleteObjects({ Delete: { Objects } }).promise();
+  },
+});
 
 Deno.test({
   name: "deleting a bucket",
-async fn(): Promise<void> {
-  await s3.deleteBucket({ Bucket: BUCKET_NAME })
-}})
+  async fn(): Promise<void> {
+    await s3.deleteBucket({ Bucket: BUCKET_NAME });
+  },
+});
